6bf863624b7ddf701e3a7610a745a5edbea6e1a2
Gabriel Rodrigues Caldas de Aquino <gabriel@labnet.nce.ufrj.br>
2020-04-07 17:37:37 -0300

    First text dialog appearing

---
+++ Assets/DisplayTextOnScreen.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayTextOnScreen : MonoBehaviour
+{
+    private GameObject dialogBG;
+    public string textToWrite;
+    public bool hasAnythingToDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+      dialogBG = GameObject.Find("Dialog_bg");
+      dialogBG.SetActive(false);
+      gameObject.SetActive(false);
+      hasAnythingToDisplay = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(hasAnythingToDisplay){
+        displayText(textToWrite);
+      }
+
+    }
+
+    void displayText(string s){
+      dialogBG.SetActive(true);
+      gameObject.SetActive(true);
+      //gameObject.GetComponent<Text>().text = s;
+
+    }
+}
---
+++ Assets/WriteTextOnScreen.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WriteTextOnScreen : MonoBehaviour
+{
+    private GameObject dialogText;
+    private GameObject dialogBG;
+    private GameObject dialogInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      dialogText = GameObject.Find("Dialog_Text");
+      dialogBG = GameObject.Find("Dialog_bg");
+      dialogInfo = GameObject.Find("Dialog_Text_info");
+      dialogBG.SetActive(false);
+      dialogText.SetActive(false);
+      dialogInfo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+        dialogBG.SetActive(false);
+        dialogText.SetActive(false);
+        dialogInfo.SetActive(false);
+        Time.timeScale = 1.0f;
+      }
+
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+      if(other.gameObject.name=="first_tile")
+      {
+        WriteOnScreen("Oh! Estou tão no alto!");
+      }
+
+
+    }
+
+    void WriteOnScreen(string s)
+    {
+        dialogBG.SetActive(true);
+        dialogText.SetActive(true);
+        dialogInfo.SetActive(true);
+        dialogText.GetComponent<Text>().text = s;
+        Time.timeScale = 0.0f;
+
+
+    }
+
+
+}
+
+/*  dialogText.GetComponent<Text>().text = "Oh! Estou tão no alto!";
+  while(!Input.GetKeyDown(KeyCode.Space)){
+    //yield return null;
+  }
+  dialogText.GetComponent<Text>().text = "Para onde devo ir?";
+  while(!Input.GetKeyDown(KeyCode.Space)){
+    //yield return null;
+  }
+  turnOnDialog = false;
+*/
